a3d9580a864a820594c4c44f04997571b57502e6
pbobyn91 <35970511+pbobyn91@users.noreply.github.com>
2018-09-13 07:55:40 -0400

    Add files via upload

--- encryption.c
+++ encryption.c
@@ -9,9 +9,22 @@ int main () {
 	gets(phrase);
 	
 	for (i = 0; ( i<256 && phrase[i]!='\0' ); i++){
-		phrase[i] = phrase[i] - 5;
+		if (phrase[i] == ' '){
+		} else {
+			phrase[i] = phrase[i] - 5;
+		}
 	}
 	
 	printf("\nEncrypted file:\n\n%s", phrase);
+	
+	for (i = 0; ( i<256 && phrase[i]!='\0' ); i++){
+		if (phrase[i] == ' '){
+		} else {
+			phrase[i] = phrase[i] + 5;
+		}
+	}
+	
+	printf("\n\nPhrase after decryption:\n\n%s", phrase);
+	
 	return 0;
 }
